5e52198f40ab018ef83343452cf85fbffd56e646
Tomislav Novoselec <tomislav.novoselec@fiveminutes.eu>
2016-05-31 18:20:25 +0200

    improve pin security

--- app/src/main/java/com/grablets/fragment/SettingsPinFragment.java
+++ app/src/main/java/com/grablets/fragment/SettingsPinFragment.java
@@ -3,21 +3,23 @@ package com.grablets.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.dpizarro.pinview.library.PinView;
 import com.grablets.R;
 import com.grablets.business.PreferenceAccessor;
 import com.grablets.di.ActivityComponent;
 import com.grablets.di.ComponentProvider;
+import com.grablets.ui.utils.SimpleTextWatcher;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import me.philio.pinentry.PinEntryView;
 
 public class SettingsPinFragment extends BaseFragment {
 
@@ -36,10 +38,10 @@ public class SettingsPinFragment extends BaseFragment {
   View settingsPinConfirmContainer;
 
   @BindView(R.id.settings_pin_enter)
-  PinView settingsPinEnter;
+  PinEntryView settingsPinEnter;
 
   @BindView(R.id.settings_pin_confirm)
-  PinView settingsPinConfirm;
+  PinEntryView settingsPinConfirm;
 
   @BindView(R.id.settings_pin_save)
   Button settingsPinSave;
@@ -67,44 +69,58 @@ public class SettingsPinFragment extends BaseFragment {
   @Override
   public void onViewCreated(View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    settingsPinEnter.setOnCompleteListener((completed, pinResults) -> settingsPinConfirm.requestFocus());
+    settingsPinEnter.addTextChangedListener(new SimpleTextWatcher() {
+      @Override
+      public void afterTextChanged(Editable s) {
+        if (s.length() == 4) {
+          settingsPinConfirm.requestFocus();
+        }
+      }
+    });
 
-    settingsPinConfirm.setOnCompleteListener((completed, pinResults) -> validatePins());
+    settingsPinConfirm.addTextChangedListener(new SimpleTextWatcher() {
+      @Override
+      public void afterTextChanged(Editable s) {
+        if (s.length() == 4) {
+          validatePins();
+        }
+      }
+    });
   }
 
   @Override
   public void onAttach(Context context) {
     super.onAttach(context);
-    if (context instanceof SettingsPinListener){
+    if (context instanceof SettingsPinListener) {
       settingsPinListener = (SettingsPinListener) context;
     }
   }
 
   @OnClick(R.id.settings_pin_save)
-  public void onSavePinClicked(){
-    String enteredPin = settingsPinEnter.getPinResults();
+  public void onSavePinClicked() {
+    String enteredPin = settingsPinEnter.getText().toString();
     preferenceAccessor.setSecurityPin(enteredPin);
-    if (settingsPinListener != null){
+    if (settingsPinListener != null) {
       settingsPinListener.onPinConfirmed();
     }
 
-    settingsPinConfirm.clear();
-    settingsPinEnter.clear();
+    settingsPinConfirm.clearText();
+    settingsPinEnter.clearText();
   }
 
   @OnClick(R.id.settings_pin_remove)
-  public void onRemovePinClicked(){
+  public void onRemovePinClicked() {
     preferenceAccessor.setSecurityPin(null);
-    if (settingsPinListener != null){
+    if (settingsPinListener != null) {
       settingsPinListener.onPinRemoved();
     }
-    settingsPinConfirm.clear();
-    settingsPinEnter.clear();
+    settingsPinConfirm.clearText();
+    settingsPinEnter.clearText();
   }
 
   private void validatePins() {
-    String enteredPin = settingsPinEnter.getPinResults();
-    String confirmedPin = settingsPinConfirm.getPinResults();
+    String enteredPin = settingsPinEnter.getText().toString();
+    String confirmedPin = settingsPinConfirm.getText().toString();
 
     settingsPinSave.setEnabled(enteredPin.equals(confirmedPin));
   }
